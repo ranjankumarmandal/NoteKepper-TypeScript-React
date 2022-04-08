67a2dfba4df3bbd3243de0b02724144a8dd60449
Ranjan Kumar Mandal <ranjancit2k15cse@gmail.com>
2022-04-08 14:40:19 +0530

    completed component AboutScreen.tsx

--- src/components/screens/AboutScreen.tsx
+++ src/components/screens/AboutScreen.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
 const AboutScreen = () => {
-  return <div>AboutScreen</div>;
+  return (
+    <div>
+      <h1>About This App</h1>
+      <h3>App Name: NotesKeeper</h3>
+      <h3>About: Store your notes online</h3>
+      <h3>
+        By -{' '}
+        <a
+          href='https://www.linkedin.com/in/ranjan-kumar-m-818367158/'
+          target='_blank'
+          style={{ textDecoration: 'none' }}
+        >
+          Ranjan Kumar Mandal
+        </a>
+      </h3>
+    </div>
+  );
 };
 
 export default AboutScreen;
